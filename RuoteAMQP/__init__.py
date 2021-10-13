a44a431ae782ae2bda90989c7fe5288424ef0f4d
David Greaves <david.greaves@jolla.com>
2021-10-13 14:03:24 +0100

    Allow the DictAttrProxy to be imported
    Signed-off-by: David Greaves <david@dgreaves.com>

--- RuoteAMQP/__init__.py
+++ RuoteAMQP/__init__.py
@@ -1,3 +1,3 @@
 from .participant import Participant
-from .workitem import FlowExpressionId, Workitem
+from .workitem import FlowExpressionId, Workitem, DictAttrProxy
 from .launcher import Launcher
